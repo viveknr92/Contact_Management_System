07aba5cd72624ccb8495945eda3c5905ebd8bf60
viveknr92 <32685466+viveknr92@users.noreply.github.com>
2018-10-10 16:24:33 -0500

    front end clean up

--- public/index.js
+++ public/index.js
@@ -1,5 +1,56 @@
-var i = -1;
+var phone_var = `<div id='phone' class='form-group' style='display:none'>\
+            <div class='form-row'>\
+                <div class='form-group col-md-3'>\
+                    <input list='PhoneType' name='phone[][phone_type]' class='form-control'>\
+                    <datalist id='PhoneType'>\
+                        <option value='home'>\
+                        <option value='work'>\
+                    </datalist>\
+                </div>\
+                <div class='form-group col-md-6'>\
+                    <input type='tel' class='form-control' id='inputPhone' name='phone[][phone]' placeholder='Phone'>\
+                </div>\
+                <div class='form-group col-md-1'>\
+                    <button type='button' class='btn btn-danger' id='deletePhone'> X </button>\
+                </div>\
+            </div>\
+        </div>`;
 
+var phone_var = `<div id='phone' class='form-group' style='display:none'>\
+            <div class='form-row'>\
+                <div class='form-group col-md-3'>\
+                    <input list='PhoneType' name='phone[][phone_type]' class='form-control'>\
+                    <datalist id='PhoneType'>\
+                        <option value='home'>\
+                        <option value='work'>\
+                    </datalist>\
+                </div>\
+                <div class='form-group col-md-6'>\
+                    <input type='tel' class='form-control' id='inputPhone' name='phone[][phone]' placeholder='Phone'>\
+                </div>\
+                <div class='form-group col-md-1'>\
+                    <button type='button' class='btn btn-danger' id='deletePhone'> X </button>\
+                </div>\
+            </div>\
+        </div>`;
+
+var phone_var = `<div id='phone' class='form-group' style='display:none'>\
+            <div class='form-row'>\
+                <div class='form-group col-md-3'>\
+                    <input list='PhoneType' name='phone[][phone_type]' class='form-control'>\
+                    <datalist id='PhoneType'>\
+                        <option value='home'>\
+                        <option value='work'>\
+                    </datalist>\
+                </div>\
+                <div class='form-group col-md-6'>\
+                    <input type='tel' class='form-control' id='inputPhone' name='phone[][phone]' placeholder='Phone'>\
+                </div>\
+                <div class='form-group col-md-1'>\
+                    <button type='button' class='btn btn-danger' id='deletePhone'> X </button>\
+                </div>\
+            </div>\
+        </div>`;
 
 $(document).ready(function (){
     //addForm('#addAddress', '#address', '#cloneAddresshere', '#deleteAddress');
@@ -13,33 +64,17 @@ $(document).ready(function (){
 
 
     $("#formID").submit(function(e) {
-        // e.preventDefault();
-        // $(this).append('<input type="hidden" name="field_name" value="value" /> ');
-        // console.log(e);
-        // console.log($(this).closest('form').serialize());
-        var phone = [];
+        //e.preventDefault();
         $('#clonePhonehere').children().each(function(e){
-            console.log($(this).find('input')[0].value);
-            console.log($(this).find('input')[1].value);
-            // var temp = {
-            //     'phone_type' : $(this).find('input')[0].value,
-            //     'phone' : $(this).find('input')[1].value
-            // };
-            var temp = {};
-            temp['phone_type'] = $(this).find('input')[0].value;
-            temp['phone'] =  $(this).find('input')[1].value;
-            // phone.push(JSON.stringify(temp));
-            phone.push(temp);
+            console.log($(this).find('input')[0].name);
+            console.log($(this).find('input')[1].name);
             
-            // console.log(JSON.stringify(temp));
+            $(this).find('input')[0].name = "phone[" + e + "][phone_type]";
+            $(this).find('input')[1].name = "phone[" + e + "][phone]";
+
+            console.log($(this).find('input')[0].name);
+            console.log($(this).find('input')[1].name);
         });
-
-        console.log(JSON.stringify(phone));
-
-        $('<input />').attr('type', 'hidden')
-          .attr('name', "phone2")
-          .attr('value', JSON.stringify(phone))
-          .appendTo('#formID');
 
         console.log($(this).closest('form').serialize());
         
@@ -49,38 +84,8 @@ $(document).ready(function (){
 });
 function addForm(AddbuttonID, formID, cloneHereID, DeletebuttonID){
     $(document).on('click', AddbuttonID, function () {
-        i += 1;
-        var phone_var = `<div id='phone' class='form-group' style='display:none'>\
-            <div class='form-row'>\
-                <div class='form-group col-md-3'>\
-                    <input list='PhoneType' name='phone[${i}][phone_type]' class='form-control'>\
-                    <datalist id='PhoneType'>\
-                        <option value='home'>\
-                        <option value='work'>\
-                    </datalist>\
-                </div>\
-                <div class='form-group col-md-6'>\
-                    <input type='tel' class='form-control' id='inputPhone' name='phone[${i}][phone]' placeholder='Phone'>\
-                </div>\
-                <div class='form-group col-md-1'>\
-                    <button type='button' class='btn btn-danger' id='deletePhone'> X </button>\
-                </div>\
-            </div>\
-        </div>`;
-        $(phone_var).appendTo(cloneHereID).show();
-
-        // $('#phone:last input').each(function() {
-        //     //console.log($(this).val());
-        //     var input = $(this).attr("name");
-        //     //console.log($(this).attr("name"));
-        //     var output = input.replace((i).toString(), i+1);
-        //     $(this).attr("name", output);
-        //     console.log('Value of i is ',i);
-        //     console.log('Name ', $(this).attr("name"));
-        // });
         
-        //$(DeletebuttonID).off();
-
+        $(phone_var).appendTo(cloneHereID).show();
         $(document).on('click', DeletebuttonID, function () {
             console.log($(this));
             $(this).parents(formID).remove();
@@ -90,11 +95,10 @@ function addForm(AddbuttonID, formID, cloneHereID, DeletebuttonID){
 }
 
 function deleteForm( formID, DeletebuttonID, formToDelete, fieldset){
-    
-    //$(DeletebuttonID).off();
-    // $(document).on('click', DeletebuttonID, function () {
-    //     console.log($(this));
-    //     $(formToDelete).after("<input type='hidden' name='" + fieldset + "_delete[]' value='" + $(this).val() + "'/>");
-    //     $(this).parents(formID).remove();
-    // });
+    $(DeletebuttonID).off();
+    $(document).on('click', DeletebuttonID, function () {
+        console.log($(this));
+        $(formToDelete).after("<input type='hidden' name='" + fieldset + "_delete[]' value='" + $(this).val() + "'/>");
+        $(this).parents(formID).remove();
+    });
 }
